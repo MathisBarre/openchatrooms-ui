b65e8c8a9e71ff255a1f83108f3b267743f2562e
Mathis Barré <mathis.barre@live.fr>
2023-05-18 23:54:29 +0200

    signup image

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -97,6 +97,34 @@ export default function Example() {
             </div>
 
             <div>
+              <p className="block text-sm font-medium leading-6 text-white">
+                Photo de profil
+              </p>
+              <div className="mt-1 h-20 bg-white/5 rounded border border-dashed border-white/10 hover:bg-white/20 cursor-pointer duration-75 flex justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-white/80 mr-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                  />
+                </svg>
+                <p className="text-white/80 text-sm">
+                  Glissez-déposez votre image ou{" "}
+                  <span className="text-roman-coffee-300 underline">
+                    naviguez
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            <div>
               <button type="submit" className="button">
                 Créer le compte
               </button>
